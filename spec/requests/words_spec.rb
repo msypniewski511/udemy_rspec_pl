9a11d21eb2f932220fbde8a37af0f38230f2cbdd
Maciej <msypniewski511@gmail.com>
2024-10-16 17:00:00 +0100

    Add base for words system spec

--- spec/requests/words_spec.rb
+++ spec/requests/words_spec.rb
@@ -1,14 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe "Words", type: :request do
+  let(:word) { Word.create!(value: 'cat', language: 'english') }
   describe "GET /words" do
     it "returns http success" do
-      get "/words"
+      get words_path
       expect(response).to have_http_status(:success)
     end
     it "render index page" do
       get "/words"
-      expect(response.body).to include("h1")
+      expect(response.body).to include(word.value)
     end
   end
 end
